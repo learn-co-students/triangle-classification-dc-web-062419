d29732725022e6278f23826bca7fa5a37a783207
Luke O'Malley <Lukeomalley95@gmail.com>
2019-06-29 21:24:12 -0400

    Completed Lab 🤙

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_reader :side1, :side2, :side3
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  class TriangleError < StandardError
+  end
+
+  def kind
+    real_triangle
+    if side1 == side2 and side2 == side3
+      return :equilateral
+    elsif side1 == side2 or side2 == side3 or side1 == side3
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  def real_triangle
+    real = [(side1 + side2 > side3), (side2 + side3 > side1), (side1 + side3 > side2)]
+    real.each {|value| if value == false then raise TriangleError end}
+  end
+
 end
