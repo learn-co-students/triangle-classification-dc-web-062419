f6cd867962874a1f7fa5b49dbcb0cbcfe7bb30de
github username <github email address>
2019-06-30 21:58:41 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
   # write code here
-end
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three 
+  end
+  def kind()
+    if (@side_1 == 0) || (@side_2 == 0) || (@side_3 == 0)
+      raise TriangleError
+    elsif @side_one + @side_two <= @side_three || @side_one + @side_three <= @side_two || @side_two + @side_three <= @side_one
+      raise TriangleError
+    else 
+    if @side_one == @side_two && @side_one == @side_three
+      :equilateral
+    elsif @side_one == @side_two || @side_one == @side_three || @side_two == @side_three
+      :isosceles
+    elsif @side_one != @side_two && @side_one != @side_three && @side_two != @side_three
+      :scalene
+      end
+    end 
+  end
+
+
+  class TriangleError < StandardError
+  
+  end 
+end 
+  
